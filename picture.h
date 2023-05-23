198f175cf27403193ff4c0d2cded106dfe16a02e
flyway1 <132738420+flyway1@users.noreply.github.com>
2023-05-23 18:44:26 +0800

    Create picture.h

---
+++ picture.h
@@ -0,0 +1,20 @@
+#ifndef PICTURE_H
+#define PICTURE_H
+
+#include<QWidget>
+#include<QLabel>
+
+class picture:public QLabel
+{
+    Q_OBJECT
+public:
+    picture(QString Img,QWidget *widget);
+    picture(QWidget *widget);
+    void changeImg(QString Img);
+signals:
+
+public slots:
+
+};
+
+#endif // PICTURE_H
